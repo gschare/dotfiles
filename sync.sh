9308cb9411338e8dd2cfc2e1691c3f98156aa12d
gschare <grschare1315@gmail.com>
2020-09-22 16:52:33 -0700

    Update Windows Terminal settings

--- sync.sh
+++ sync.sh
@@ -4,3 +4,4 @@ cp ~/.aliases bash/.aliases
 cp ~/.bash_profile bash/.bash_profile
 cp ~/.nanorc nano/.nanorc
 cp ~/.vimrc vim/.vimrc
+cp /mnt/c/Users/grsch/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json WindowsTerminal/settings.json

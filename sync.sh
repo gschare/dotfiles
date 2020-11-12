576241257af3af91f9a4b9ba0a97af4554763aa0
gschare <grschare1315@gmail.com>
2020-11-11 23:13:57 -0800

    Remove things that are not actually dotfiles (and nano because I don't use nano)

--- sync.sh
+++
@@ -1,7 +0,0 @@
-cp ~/.bashrc bash/.bashrc
-cp ~/.functions bash/.functions
-cp ~/.aliases bash/.aliases
-cp ~/.bash_profile bash/.bash_profile
-cp ~/.nanorc nano/.nanorc
-cp ~/.vimrc vim/.vimrc
-cp /mnt/c/Users/grsch/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json WindowsTerminal/settings.json

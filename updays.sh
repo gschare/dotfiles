8f883cc4f739c81ee64afa1602515eed0ebcada4
gschare <grschare1315@gmail.com>
2021-06-27 02:09:14 -0400

    add decimal value to updays

--- updays.sh
+++ updays.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "($(date +%s)-$(date +%s -d $(uptime -s | cut -d' ' -f1 | tr -d '-')))/(3600*24)" | bc
+echo "scale=2;($(date +%s)-$(date +%s -d $(uptime -s | cut -d' ' -f1 | tr -d '-')))/(3600*24)" | bc

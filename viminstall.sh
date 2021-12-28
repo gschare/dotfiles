4b7104d4edcd26c96fad52075538cc877dc65e44
Gregory Schare <grschare1315@gmail.com>
2021-12-28 00:10:37 -0800

    add vim plugins install script

---
+++ viminstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ~
+mkdir -p .vim/pack/dist/start
+cd .vim/pack/dist/start
+git clone https://github.com/morhetz/gruvbox
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
+git clone https://github.com/morhetz/gruvbox

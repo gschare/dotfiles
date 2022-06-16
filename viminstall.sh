ea922d3feb0880917a2a53a2607e0d1014d94c14
Gregory Schare <grschare1315@gmail.com>
2022-06-16 17:05:40 -0400

    update viminstall script

--- viminstall.sh
+++ viminstall.sh
@@ -6,3 +6,4 @@ git clone https://github.com/morhetz/gruvbox
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes
 git clone https://github.com/morhetz/gruvbox
+git clone https://github.com/neovimhaskell/haskell-vim
